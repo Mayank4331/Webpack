094ca46f70597560c5996af68c0b088efeb1b387
MGuptaB <mayankgupta93923@gmail.com>
2024-04-28 14:44:18 +0530

    webpack config

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import img from './assets/image.svg'
+
+const arr = [1, 2, 3];
+arr.push(2);
+console.log(arr);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'assets',
+                    },
+                },
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    mode: "development",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3000,
+        open: true,
+        hot: true
+    }
+};
